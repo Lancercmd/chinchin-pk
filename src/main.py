a0b3f03f988074cf2c6145f955251236c5df5cda
fz6m <59400654+fz6m@users.noreply.github.com>
2023-02-22 09:22:37 +0800

    chore: v2.6.2

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ KEYWORDS = {
     "help": ["牛子帮助"],
 }
 
-VERSION = '2.6.1'
+VERSION = '2.6.2'
 HELPPER = f"牛了个牛 v{VERSION}\n可用的指令/功能有：\n" + "、".join(
     [
         KEYWORDS.get("sign_up")[0],
@@ -75,6 +75,8 @@ def message_processor(
     TODO: 物品系统
     TODO: 抽取 utils 文件的导入
     TODO: 牛子共享排行榜
+
+    TODO: 长度多的人修炼可能失败
     """
     # lazy init database
     lazy_init_database()
